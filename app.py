7bb00300c0bd47bd4219a4900e057e05851f1bd8
ccvetanska <ccvetanska@gmail.com>
2025-06-20 21:19:50 +0300

    minor changes in UI

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import torch
-import pickle
 from recommend_funcs import recommend_autoencoder, recommend_matrix, recommend_bce, recommend_svd, ratings_df, num_movies, num_users, movies_df
 from model_defs import Autoencoder, MatrixFactorizationWithRegularization
 from surprise import dump
@@ -21,20 +20,20 @@ model_matrix.eval()
 movie_ids = ratings_df['movieId'].unique()
 
 
+st.image("assets/movie.png", width=1000)
 st.title("Movie Recommender System")
+st.sidebar.header("Settings")
+selected_model = st.selectbox("Choose model", ["Autoencoder", "Matrix Factorization", "SVD"])
+user_id = st.sidebar.selectbox("Choose user", sorted(ratings_df['userId'].unique()))
 
-st.sidebar.header("Настройки")
-selected_model = st.selectbox("Избери модел", ["Autoencoder", "Matrix Factorization", "BCE", "SVD"])
-user_id = st.sidebar.selectbox("Избери потребител", sorted(ratings_df['userId'].unique()))
+n = st.sidebar.slider("Recommendations count", 1, 20, 10)
 
-n = st.sidebar.slider("Брой препоръки", 1, 20, 10)
-
-st.subheader(f"Препоръки за потребител {user_id} чрез {selected_model}")
+st.subheader(f"Recommendations for user {user_id} with {selected_model}")
 
 if selected_model == "Autoencoder":
     recs = recommend_autoencoder(user_id, model_ae, movie_ids, n)
 elif selected_model == "Matrix Factorization":
-    recs = recommend_matrix(user_id, model_matrix, movie_ids, n)
+    recs = recommend_matrix(user_id, model_matrix, n)
 # elif selected_model == "BCE":
 #     recs = recommend_bce(user_id, model_bce, n)
 elif selected_model == "SVD":
